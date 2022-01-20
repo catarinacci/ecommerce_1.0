20603041915c3f79659b2d267a7567fc4dd91f59
catarinacci <catarinacci@gmail.com>
2022-01-20 13:44:01 -0300

    vista-carro-de-compras

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,12 @@ use App\Models\Category;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Http\Livewire\ShoppingCart;
 
 
 Route::get('/', WelcomeController::class);
 Route::get('search', SearchController::class)->name('search');
+Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
 Route::get('categories/{category}',[CategoryController::class, 'show'])->name('categories.show');
 Route::get('products/{product}',[ProductController::class, 'show'])->name('products.show');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
